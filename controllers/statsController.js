91d0fa68b2130754b73547bf772341de1a33dad3
UniqueNameforMe <96441531+UniqueNameforMe@users.noreply.github.com>
2023-05-16 09:22:39 +0530

    Update statsController.js

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -4,7 +4,6 @@ exports.createStats = async (req, res) => {
   try {
     const { money, progress, days } = req.body;
 
-    // Validate the 'money' field
     if (!money || typeof money !== 'number' ) {
       return res.status(400).json({ error: 'Invalid or null input for money.' });
     }
@@ -12,7 +11,6 @@ exports.createStats = async (req, res) => {
       return res.status(400).json({ error: 'Money must be a positive value.' });
     }
 
-    // Validate the 'progress' field
     if (!progress || typeof progress !== 'number') {
       return res.status(400).json({ error: 'Invalid or null input for progress.' });
     }
@@ -20,7 +18,6 @@ exports.createStats = async (req, res) => {
       return res.status(400).json({ error: 'Progress must be between 0 and 100.' });
     }
 
-    // Validate the 'days' field
     if (!days || typeof days !== 'number') {
       return res.status(400).json({ error: 'Invalid or null input for days.' });
     }
@@ -28,17 +25,14 @@ exports.createStats = async (req, res) => {
       return res.status(400).json({ error: 'The number of days must be a positive value.' }); 
     }
 
-    // Create a new stats object
     const stats = new Stats({
       money,
       progress,
       days,
     });
 
-    // Save the stats to the database
     const savedStats = await stats.save();
 
-    // Return the saved stats object
     res.json(savedStats);
   } catch (err) {
     console.error(err);
